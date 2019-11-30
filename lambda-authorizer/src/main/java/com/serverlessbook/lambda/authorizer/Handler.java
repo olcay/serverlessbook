4e72973e6e9f509f4aa1a3a716dd599fa3e827c0
olcay <olcaybayram@gmail.com>
2019-12-01 00:55:24 +0100

    chapter3

---
+++ lambda-authorizer/src/main/java/com/serverlessbook/lambda/authorizer/Handler.java
@@ -0,0 +1,23 @@
+package com.serverlessbook.lambda.authorizer;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.serverlessbook.lambda.LambdaHandler;
+import com.serverlessbook.lambda.authorizer.models.AuthorizationInput;
+import com.serverlessbook.lambda.authorizer.models.AuthorizationOutput;
+import com.serverlessbook.lambda.authorizer.models.policy.PolicyDocument;
+import com.serverlessbook.lambda.authorizer.models.policy.PolicyStatement;
+
+public class Handler extends LambdaHandler<AuthorizationInput, AuthorizationOutput> {
+
+    @Override
+    public AuthorizationOutput handleRequest(AuthorizationInput input, Context context) {
+        final String authenticationToken = input.getAuthorizationToken();
+        final PolicyDocument policyDocument = new PolicyDocument();
+        PolicyStatement.Effect policyEffect = "serverless".equals(authenticationToken) ? 
+            PolicyStatement.Effect.ALLOW : PolicyStatement.Effect.DENY;
+
+        policyDocument.withPolicyStatement(new PolicyStatement("execute-api:Invoke",
+                policyEffect, input.getMethodArn()));
+        return new AuthorizationOutput("1234", policyDocument);
+    }
+}
