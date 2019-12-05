52b00e46657bdaf8d0b50da017bd4da11f8f68d6
olcay <olcaybayram@gmail.com>
2019-12-05 01:34:29 +0100

    file uploaded topic is added

---
+++ lambda-imageresizer/src/main/java/com/serverlessbook/lambda/imageresizer/DependencyInjectionModule.java
@@ -0,0 +1,10 @@
+package com.serverlessbook.lambda.imageresizer;
+
+import com.google.inject.AbstractModule;
+
+public class DependencyInjectionModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+    }
+}
--- lambda-imageresizer/src/main/java/com/serverlessbook/lambda/imageresizer/Handler.java
+++ lambda-imageresizer/src/main/java/com/serverlessbook/lambda/imageresizer/Handler.java
@@ -7,15 +7,41 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+import javax.inject.Inject;
 
 public class Handler implements RequestHandler<S3Event, Void> {
+
+  private static final Injector INJECTOR = Guice.createInjector(new DependencyInjectionModule());
 
   LambdaLogger logger;
 
   final AmazonS3 s3client;
 
+  private AmazonSNSClient amazonSNSClient;
+
+  @Inject
+  public Handler setAmazonSNSClient(AmazonSNSClient amazonSNSClient) {
+      this.amazonSNSClient = amazonSNSClient;
+      return this;
+  }
+
+  private void notifySnsSubscribers() {
+    try {
+       logger.log(System.getenv("FileUploadSnsTopic"));
+      amazonSNSClient.publish(System.getenv("FileUploadSnsTopic"), "new file uploaded");
+      logger.log("SNS notification sent for a new file");
+    } catch (Exception anyException) {
+      logger.log("SNS notification failed for a new file; "+ anyException.getMessage());
+    }
+  }
+
   public Handler() {
     s3client = new AmazonS3Client(new DefaultAWSCredentialsProviderChain());
+    INJECTOR.injectMembers(this);
   }
 
   private void resizeImage(String bucket, String key) {
@@ -39,6 +65,7 @@ public class Handler implements RequestHandler<S3Event, Void> {
     input.getRecords().forEach(s3EventNotificationRecord ->
         resizeImage(s3EventNotificationRecord.getS3().getBucket().getName(),
             s3EventNotificationRecord.getS3().getObject().getKey()));
+    notifySnsSubscribers();
     return null;
   }
 }
